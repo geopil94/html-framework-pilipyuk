61c09ce854a52fe5a0d4442de3a8cce9ad0818c4
George <geopil94@gmail.com>
2018-10-04 16:01:00 +0300

    фреймворк на базе bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,14 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('./assets/**/*.scss', ['sass']);
 });
+
+var autoprefixer = require('gulp-autoprefixer');
+ 
+gulp.task('prefix', function () {
+    return gulp.src('index.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./'));
+});
